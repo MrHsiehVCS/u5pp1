3cd03bd4f42dc465a02e4c1137fda1756d52ec41
Hsieh <erich@erichMacBookAir.local>
2022-12-07 15:49:04 -0800

    add tests

--- src/test/java/u5pp1/Connect4Test.java
+++ src/test/java/u5pp1/Connect4Test.java
@@ -1,65 +1,39 @@
 package u5pp1;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.List;
 
 import org.junit.jupiter.api.*;
 
-
 public class Connect4Test {
-    
-	private  int[][] BOARD_EMPTY = new int[6][7];
 
-	private  int[][] BOARD_FULL_RED = {
-		{1,1,1,1,1,1,1},
-		{1,1,1,1,1,1,1},
-		{1,1,1,1,1,1,1},
-		{1,1,1,1,1,1,1},
-		{1,1,1,1,1,1,1},
-		{1,1,1,1,1,1,1},
-	};
-	private  int[][] BOARD_FULL_BLACK = {
-		{-1,-1,-1,-1,-1,-1,-1},
-		{-1,-1,-1,-1,-1,-1,-1},
-		{-1,-1,-1,-1,-1,-1,-1},
-		{-1,-1,-1,-1,-1,-1,-1},
-		{-1,-1,-1,-1,-1,-1,-1},
-		{-1,-1,-1,-1,-1,-1,-1},
+	private int[][] BOARD_EMPTY = new int[6][7];
+
+	private int[][] BOARD_FULL_RED = {
+			{ 1, 1, 1, 1, 1, 1, 1 },
+			{ 1, 1, 1, 1, 1, 1, 1 },
+			{ 1, 1, 1, 1, 1, 1, 1 },
+			{ 1, 1, 1, 1, 1, 1, 1 },
+			{ 1, 1, 1, 1, 1, 1, 1 },
+			{ 1, 1, 1, 1, 1, 1, 1 },
 	};
 
-	private  int[][] BOARD_FULL_SPLIT = {
-		{-1,-1,-1,-1,1,1,1},
-		{1,1,1,-1,-1,-1,-1},
-		{-1,-1,-1,-1,1,1,1},
-		{1,1,1,-1,-1,-1,-1},
-		{-1,-1,-1,-1,1,1,1},
-		{1,1,1,-1,-1,-1,-1},
+	private int[][] BOARD_FULL_BLACK = {
+			{ -1, -1, -1, -1, -1, -1, -1 },
+			{ -1, -1, -1, -1, -1, -1, -1 },
+			{ -1, -1, -1, -1, -1, -1, -1 },
+			{ -1, -1, -1, -1, -1, -1, -1 },
+			{ -1, -1, -1, -1, -1, -1, -1 },
+			{ -1, -1, -1, -1, -1, -1, -1 },
 	};
 
-	private  int[][] BOARD_FULL_CHECKERED = {
-		{-1,1,-1,1,-1,1,-1},
-		{1,-1,1,-1,1,-1,1},
-		{-1,1,-1,1,-1,1,-1},
-		{1,-1,1,-1,1,-1,1},
-		{-1,1,-1,1,-1,1,-1},
-		{1,-1,1,-1,1,-1,1},
+	private int[][] BOARD_FULL_CHECKERED = {
+			{ -1, 1, -1, 1, -1, 1, -1 },
+			{ 1, -1, 1, -1, 1, -1, 1 },
+			{ -1, 1, -1, 1, -1, 1, -1 },
+			{ 1, -1, 1, -1, 1, -1, 1 },
+			{ -1, 1, -1, 1, -1, 1, -1 },
+			{ 1, -1, 1, -1, 1, -1, 1 },
 	};
-
-	@BeforeAll
-	public static void setUp() {
-		
-	}
-
-	/*
-	 * isFull 
-	 * - half black half red
-	 * - empty
-	 * - everything except 1 corner
-	 * - everything except other corner
-	 * */ 
 
 	@Test
 	public void isFull_fullBoard_returnsTrue() {
@@ -84,147 +58,469 @@ public class Connect4Test {
 		assertFalse(Connect4.isFull(board2));
 	}
 
-
-
-	/*
-	 * isBoardValid
-	 * - col of 6,5,4,3,2,1
-	 * - empty
-	 * - full
-	 * - 1 piece floating (r2, col 1)
-	 * - 1 piece floating (row max-1, col max)
-	 * - full, but 1 piece missing (row 2, col 1)
-	 * - full, but 1 piece missing (max row, max col)
-	 */ 
-
-	 @Test
-	 public void isBoardValid_fullBoard_returnsTrue() {
+	@Test
+	public void isBoardValid_fullBoard_returnsTrue() {
 		assertTrue(Connect4.isBoardValid(BOARD_FULL_BLACK));
 		assertTrue(Connect4.isBoardValid(BOARD_FULL_RED));
 		assertTrue(Connect4.isBoardValid(BOARD_FULL_CHECKERED));
-		assertTrue(Connect4.isBoardValid(BOARD_FULL_SPLIT));
-	 }
+	}
 
-	 @Test
-	 public void isBoardValid_emptyBoard_returnsTrue() {
+	@Test
+	public void isBoardValid_emptyBoard_returnsTrue() {
 		assertTrue(Connect4.isBoardValid(BOARD_EMPTY));
-	 }
+	}
 
-	 @Test
-	 public void isBoardValid_stairsBoard_returnsTrue() {
-		 int[][] boardStairs = {
-			{1,0,0,0,0,0,0},
-			{1,1,0,0,0,0,0},
-			{1,1,1,0,0,0,0},
-			{1,1,1,1,0,0,0},
-			{1,1,1,1,1,0,0},
-			{1,1,1,1,1,1,0},
+	@Test
+	public void isBoardValid_stairsBoard_returnsTrue() {
+		int[][] boardStairs = {
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 1, 0, 0, 0, 0, 0 },
+				{ 1, 1, 1, 0, 0, 0, 0 },
+				{ 1, 1, 1, 1, 0, 0, 0 },
+				{ 1, 1, 1, 1, 1, 0, 0 },
+				{ 1, 1, 1, 1, 1, 1, 0 },
 		};
-		
+
 		assertTrue(Connect4.isBoardValid(boardStairs));
-	 }
+	}
 
-	 @Test
-	 public void isBoardValid_floatingPieceBoard_returnsFalse() {
-		 int[][] board1 = {
-			{1,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
+	@Test
+	public void isBoardValid_floatingPieceBoard_returnsFalse() {
+		int[][] board1 = {
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
 		};
 
 		int[][] board2 = {
-			{0,0,0,0,0,0,0},
-			{0,0,0,1,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
 		};
 
 		int[][] board3 = {
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,1},
-			{0,0,0,0,0,0,0},
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
 		};
-		
+
 		assertFalse(Connect4.isBoardValid(board1));
 		assertFalse(Connect4.isBoardValid(board2));
 		assertFalse(Connect4.isBoardValid(board3));
-	 }
+	}
 
-	 @Test
-	 public void isBoardValid_nearlyFullBoard_returnsFalse() {
-		 int[][] board1 = {
-			{1,1,1,1,1,1,1},
-			{0,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
+	@Test
+	public void isBoardValid_nearlyFullBoard_returnsFalse() {
+		int[][] board1 = {
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
 		};
 
 		int[][] board2 = {
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,0},
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0 },
 		};
 
 		int[][] board3 = {
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,0,1,1,1},
-			{1,1,1,1,1,1,1},
-			{1,1,1,1,1,1,1},
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 0, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
 		};
-		
+
 		assertFalse(Connect4.isBoardValid(board1));
 		assertFalse(Connect4.isBoardValid(board2));
 		assertFalse(Connect4.isBoardValid(board3));
-	 }
+	}
 
+	@Test
+	public void getWinner_horizontalRedWin_returnsRedWin() {
+		int[][] board1 = {
+				{ 1, 1, 1, 1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
 
-	/*
-	 * getWinner
-	 * 
-	 * - black
-	 *   - horz (high, col 1)
-	 *   - vert (high, col 1)
-	 *   - diag up right (simple board)
-	 *   - diag up left (full board)
-	 * 
-	 * - red
-	 *   - horz (low, col max)
-	 *   - vert (low, col max)
-	 *   - diag up right (simple board)
-	 *   - diag up left (full board)
-	 * 
-	 * - tie
-	 *   - vert black, horz red
-	 *   - diagonal black, diagonal red, full board
-	 * 
-	 * - no winner
-	 *   - full board (lots of 3 in a rows)
-	 *   - empty board
-	 */
+		int[][] board2 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 1, 1, 1, 1, 1, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
 
-	 private int[][] copyBoard(int[][] board) {
+		int[][] board3 = {
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 1, 1, 1 },
+		};
+
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board1));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board2));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board3));
+	}
+
+	@Test
+	public void getWinner_verticalRedWin_returnsRedWin() {
+		int[][] board1 = {
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board2 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board3 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+		};
+
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board1));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board2));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board3));
+	}
+
+	@Test
+	public void getWinner_diagonalUpRedWin_returnsRedWin() {
+		int[][] board1 = {
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 1, 0, 0, 0, 0, 0 },
+				{ 0, 0, 1, 0, 0, 0, 0 },
+				{ 1, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board2 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0 },
+				{ 1, 0, 0, 1, 0, 0, 0 },
+				{ 0, 1, 0, 0, 0, 0, 0 },
+				{ 0, 0, 1, 1, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0 },
+		};
+
+		int[][] board3 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 1 },
+				{ 0, 0, 0, 0, 1, 0, 1 },
+				{ 0, 0, 0, 0, 0, 1, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+		};
+
+		int[][] board4 = {
+				{ 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 1, 0, 0 },
+				{ 0, 0, 0, 1, 0, 1, 1 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 1, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+		};
+
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board1));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board2));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board3));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board4));
+	}
+
+	@Test
+	public void getWinner_diagonalDownRedWin_returnsRedWin() {
+		int[][] board1 = {
+				{ 1, 0, 0, 1, 0, 0, 0 },
+				{ 1, 0, 1, 0, 0, 0, 0 },
+				{ 0, 1, 1, 0, 0, 0, 0 },
+				{ 1, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board2 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0 },
+				{ 1, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 1, 0, 0, 0, 0 },
+				{ 0, 1, 0, 1, 0, 0, 0 },
+				{ 1, 0, 0, 1, 0, 0, 0 },
+		};
+
+		int[][] board3 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 1, 1 },
+				{ 0, 0, 0, 0, 1, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 1 },
+		};
+
+		int[][] board4 = {
+				{ 0, 0, 0, 1, 0, 0, 1 },
+				{ 0, 0, 0, 0, 1, 1, 0 },
+				{ 0, 0, 0, 1, 1, 0, 1 },
+				{ 0, 0, 0, 1, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 1, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+		};
+
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board1));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board2));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board3));
+		assertEquals(Connect4.RED_WIN, Connect4.getWinner(board4));
+	}
+
+	@Test
+	public void getWinner_horizontalBlackWin_returnsBlackWin() {
+		int[][] board1 = {
+				{ -1, -1, -1, -1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board2 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, -1, -1, -1, -1, -1, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board3 = {
+				{ -1, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, -1, -1, -1, -1 },
+		};
+
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board1));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board2));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board3));
+	}
+
+	@Test
+	public void getWinner_verticalBlackWin_returnsBlackWin() {
+		int[][] board1 = {
+				{ -1, 0, 0, 0, 0, 0, 0 },
+				{ -1, 0, 0, 0, 0, 0, 0 },
+				{ -1, 0, 0, 0, 0, 0, 0 },
+				{ -1, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board2 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board3 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, -1 },
+				{ 0, 0, 0, 0, 0, 0, -1 },
+				{ 0, 0, 0, 0, 0, 0, -1 },
+				{ 0, 0, 0, 0, 0, 0, -1 },
+		};
+
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board1));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board2));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board3));
+	}
+
+	@Test
+	public void getWinner_diagonalUpBlackWin_returnsBlackWin() {
+		int[][] board1 = {
+				{ -1, 0, 0, 0, 0, 0, 0 },
+				{ -1, -1, 0, 0, 0, 0, 0 },
+				{ 0, 0, -1, 0, 0, 0, 0 },
+				{ -1, 0, 0, -1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board2 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, 0 },
+				{ -1, 0, 0, -1, 0, 0, 0 },
+				{ 0, -1, 0, 0, 0, 0, 0 },
+				{ 0, 0, -1, -1, 0, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, 0 },
+		};
+
+		int[][] board3 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, -1 },
+				{ 0, 0, 0, 0, -1, 0, -1 },
+				{ 0, 0, 0, 0, 0, -1, 0 },
+				{ 0, 0, 0, 0, 0, 0, -1 },
+		};
+
+		int[][] board4 = {
+				{ 0, 0, 0, -1, 0, 0, 0 },
+				{ 0, 0, 0, 0, -1, 0, 0 },
+				{ 0, 0, 0, -1, 0, -1, -1 },
+				{ 0, 0, 0, 0, 0, 0, -1 },
+				{ 0, 0, 0, 0, 0, -1, 0 },
+				{ 0, 0, 0, 0, 0, 0, -1 },
+		};
+
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board1));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board2));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board3));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board4));
+	}
+
+	@Test
+	public void getWinner_diagonalDownBlackWin_returnsBlackWin() {
+		int[][] board1 = {
+				{ -1, 0, 0, -1, 0, 0, 0 },
+				{ -1, 0, -1, 0, 0, 0, 0 },
+				{ 0, -1, -1, 0, 0, 0, 0 },
+				{ -1, 0, 0, -1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board2 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, 0 },
+				{ -1, 0, 0, -1, 0, 0, 0 },
+				{ 0, 0, -1, 0, 0, 0, 0 },
+				{ 0, -1, 0, -1, 0, 0, 0 },
+				{ -1, 0, 0, -1, 0, 0, 0 },
+		};
+
+		int[][] board3 = {
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, -1 },
+				{ 0, 0, 0, 0, 0, -1, -1 },
+				{ 0, 0, 0, 0, -1, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, -1 },
+		};
+
+		int[][] board4 = {
+				{ 0, 0, 0, -1, 0, 0, -1 },
+				{ 0, 0, 0, 0, -1, -1, 0 },
+				{ 0, 0, 0, -1, -1, 0, -1 },
+				{ 0, 0, 0, -1, 0, 0, -1 },
+				{ 0, 0, 0, 0, 0, -1, 0 },
+				{ 0, 0, 0, 0, 0, 0, -1 },
+		};
+
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board1));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board2));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board3));
+		assertEquals(Connect4.BLACK_WIN, Connect4.getWinner(board4));
+	}
+
+	@Test
+	public void getWinner_tie_returnsBothWin() {
+		int[][] board1 = {
+				{ -1, 0, 0, -1, 0, 0, 0 },
+				{ -1, 0, -1, 0, 0, 0, 0 },
+				{ 0, -1, -1, 0, 0, 0, 0 },
+				{ -1, 0, 0, -1, 0, 0, 0 },
+				{ 1, 1, 1, 1, 1, 1, 1 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+		};
+
+		int[][] board2 = {
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, -1, 0, 1, 0 },
+				{ -1, 0, 0, 0, 1, 0, 0 },
+				{ 0, 0, -1, 1, 0, 0, 0 },
+				{ -1, -1, -1, -1, 0, 0, 0 },
+				{ -1, 0, 0, -1, 0, 0, 0 },
+		};
+
+		int[][] board3 = {
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 0, 1, 0, 0, 0, 0, 0 },
+				{ 0, 0, 1, -1, 0, 0, -1 },
+				{ 0, 0, 0, 1, 0, -1, -1 },
+				{ 0, 0, 0, 0, -1, 0, 0 },
+				{ 0, 0, 0, -1, 0, 0, -1 },
+		};
+
+		assertEquals(Connect4.BOTH_WIN, Connect4.getWinner(board1));
+		assertEquals(Connect4.BOTH_WIN, Connect4.getWinner(board2));
+		assertEquals(Connect4.BOTH_WIN, Connect4.getWinner(board3));
+		assertEquals(Connect4.BOTH_WIN, Connect4.getWinner(BOARD_FULL_CHECKERED));
+	}
+
+	@Test
+	public void getWinner_noWinnerBoard_returnsNoWinner() {
+		int[][] board1 = {
+			{ 1, 1, 1, 0,-1,-1,-1},
+			{-1,-1,-1, 0, 1, 1, 1},
+			{ 1, 1, 1, 0,-1,-1,-1},
+			{-1,-1,-1, 0, 1, 1, 1},
+			{ 1, 1, 1, 0,-1,-1,-1},
+			{-1,-1,-1, 0, 1, 1, 1},
+		};
+		assertEquals(Connect4.NO_WINNER, Connect4.getWinner(BOARD_EMPTY));
+		assertEquals(Connect4.NO_WINNER, Connect4.getWinner(board1));
+	}
+
+	private int[][] copyBoard(int[][] board) {
 		int[][] output = new int[board.length][board[0].length];
-		for(int r = 0; r < board.length; r++) {
-			for(int c = 0; c < board[r].length; c++) {
+		for (int r = 0; r < board.length; r++) {
+			for (int c = 0; c < board[r].length; c++) {
 				output[r][c] = board[r][c];
 			}
 		}
 		return output;
-	 }
+	}
 
 }
